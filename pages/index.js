bba862c9f875dd15037725644d221e911bb673d0
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-05-18 18:01:56 -0300

    ini

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Featured from '@/components/Featured';
 import Header from '@/components/Header';
-import Product from '@/models/Product';
+// import Product from '@/models/Product';
 import { mongooseConnectShared } from './../shared/mongooseShared';
 import NewProducts from '@/components/NewProducts';
 import Layout from '@/components/Layout';
@@ -22,19 +22,19 @@ export default function HomePage({ featuredProduct, newProducts }) {
   );
 }
 
-export async function getServerSideProps() {
-  const featuredProductId = '663bbdf58cf7d3a4cd34950a';
-  await mongooseConnectShared();
-  const featuredProduct = await Product.findById(featuredProductId);
-  const newProducts = await Product.find({}, null, {
-    sort: { _id: -1 },
-    limit: 10,
-  });
-  return {
-    // stringfy turns into string, then parse to convert into objct
-    props: {
-      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
-      newProducts: JSON.parse(JSON.stringify(newProducts)),
-    },
-  };
-}
+// export async function getServerSideProps() {
+//   const featuredProductId = '663bbdf58cf7d3a4cd34950a';
+//   await mongooseConnectShared();
+//   const featuredProduct = await Product.findById(featuredProductId);
+//   const newProducts = await Product.find({}, null, {
+//     sort: { _id: -1 },
+//     limit: 10,
+//   });
+//   return {
+//     // stringfy turns into string, then parse to convert into objct
+//     props: {
+//       featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+//       newProducts: JSON.parse(JSON.stringify(newProducts)),
+//     },
+//   };
+// }
