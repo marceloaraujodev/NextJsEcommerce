139405db24d12a96356e9886b601b3a8cd56e652
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-05-18 16:25:28 -0300

    initial

--- admin/pages/api/orders.js
+++
@@ -1,9 +0,0 @@
-import { mongooseConnect } from "@/lib/mongoose";
-import Order from "@/models/Order";
-
-
-export default async function ordersHandler(req, res) {
-  await mongooseConnect();
-  const order = await Order.find().sort({createdAt:-1});
-  res.json(order)
-}
