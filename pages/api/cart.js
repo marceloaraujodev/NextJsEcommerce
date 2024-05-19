ead6dc2a1cf30ea22d4cc46d6f0f614076d8f913
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-05-19 13:25:14 -0300

    api

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -1,9 +1,10 @@
-import { mongooseConnect } from "@/lib/mongoose";
+// import { mongooseConnect } from "@/lib/mongoose";
+import { mongooseConnectShared } from './../../shared/mongooseShared'
 import Product from "@/models/Product";
 
 
 export default async function cartHandler(req, res) {
-  await mongooseConnect();
+  await mongooseConnectShared();
   const ids = req.body.ids
   res.json(await Product.find({_id: ids}));
 }
